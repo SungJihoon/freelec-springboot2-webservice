7f73096eeb93b1fec41cf83d91f454958f74ac43
Jihoon Sung <sungjihoon@gmail.com>
2020-10-20 00:07:58 +0900

    스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현

--- src/main/java/net/bbada/book/springboot/web/IndexController.java
+++ src/main/java/net/bbada/book/springboot/web/IndexController.java
@@ -1,12 +1,17 @@
 package net.bbada.book.springboot.web;
 
 import lombok.RequiredArgsConstructor;
+import net.bbada.book.springboot.config.auth.LoginUser;
+import net.bbada.book.springboot.config.auth.dto.SessionUser;
+import net.bbada.book.springboot.domain.user.User;
 import net.bbada.book.springboot.service.PostsService;
 import net.bbada.book.springboot.web.dto.PostsResponseDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,8 +20,12 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
